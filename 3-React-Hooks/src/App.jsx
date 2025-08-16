f193499d1fc89f7b4be4935e27ab91226f481436
Narottam Sharma <narottamdevengineer@gmail.com>
2025-08-16 12:37:09 +0000

    Refactor App component to manage count and friends; remove unused imports and initial content

--- 3-React-Hooks/src/App.jsx
+++ 3-React-Hooks/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [count, setCount] = useState(100);
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    console.log(count);
+    const [friends,setFriends]=useState(['Sita','Durga'])
+    const addOneFriend=()=>setFriends([...friends,'Narottam'])
+    const RemoveOneFriend = ()=>setFriends(friends.filter(f=>f!=='Durga'))
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <h1>
+              {friends.map((f)=>(
+                <li key={f}>{f}</li>
+              ))}
+            </h1>
+            <button onClick={addOneFriend}>Add Friend</button>
+            <button onClick={RemoveOneFriend}>Remove Friend</button>
+        </div>
+    );
+};
 
-export default App
+export default App;
