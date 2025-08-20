7c4cad68b257e74ec58ad943b2e83d5cce25c9ed
Narottam Sharma <narottamphodegaa@gmail.com>
2025-08-20 09:55:45 +0530

    chore: remove unused React Portal project files and configurations

--- 3-React-Hooks/src/components/Switcher.jsx
+++ 3-React-Hooks/src/components/Switcher.jsx
@@ -4,7 +4,7 @@ function Switcher() {
   const [sw, setSw] = useState(false);
   return <div>{sw ? <span>Dark</span> : <span>Light</span>}
   <br />
-  <input type="text" />
+  <input type="text" key={sw?"dark":"light"} />
   <button onClick={()=>setSw(s=>!s)} >Switch</button>
   </div>;
 }
