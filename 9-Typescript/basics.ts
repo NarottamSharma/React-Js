9a25eb9bc9cc6ce471ac26e94874b39637f79c48
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-02 11:50:12 +0530

    refactor: remove old TypeScript app files and add new basics.ts with updated code

---
+++ 9-Typescript/basics.ts
@@ -0,0 +1,38 @@
+let myName : string ='Narottam'
+myName =  'Akhila'
+console.log(myName);
+
+const favNumber : number = 12;
+// favNumber+=2;
+console.log(favNumber);
+
+function d() {
+  console.log("Hi");
+}
+d();
+let name: string = "Hi ji Narottam";
+name = "78";
+
+let tsHard:boolean = false;
+console.log(tsHard);
+tsHard = true;
+console.log(tsHard);
+
+let tech = 'typescript';
+// tech =12;
+
+// any type
+let color: any = 'crimson-red'
+console.log(color);
+color = 12;
+console.log(color);
+
+// Regular function 
+function addOne(num:number){
+  return num+1;
+}
+console.log(addOne(3));
+
+const double = (x:number,y:number) => x*y;
+console.log(double(2,3));
+
