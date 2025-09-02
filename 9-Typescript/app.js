3883455bcd24bc4a7763c043cc5ddf9b0cda0f52
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-02 11:09:21 +0530

    feat: add TypeScript support with initial app structure and configuration

---
+++ 9-Typescript/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+var b = 12;
+var b = 13;
+function d() {
+    console.log("Hi");
+}
+a;
+let name = "Hi ji Narottam";
+name = 78;
+//# sourceMappingURL=app.js.map
---
+++ 9-Typescript/app.ts
@@ -0,0 +1,13 @@
+let myName : string ='Narottam'
+myName =  'Akhila'
+
+let favNumber : number = 12;
+favNumber+=2;
+console.log(favNumber);
+
+function d() {
+  console.log("Hi");
+}
+d();
+let name: string = "Hi ji Narottam";
+name = "78";
