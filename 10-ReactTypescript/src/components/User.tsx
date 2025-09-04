4c82e27264fd69dedba283659d833cea92076012
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-04 16:27:07 +0530

    refactor: Remove unused assets and styles; simplify App component structure

---
+++ 10-ReactTypescript/src/components/User.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// {name,age,isStudent}
+const User = (props:{name:string;age:number;isStudent:boolean} ) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h2>{props.age}</h2>
+      <h2>{props.isStudent}</h2>
+    </div>
+  )
+}
+
+export default User
