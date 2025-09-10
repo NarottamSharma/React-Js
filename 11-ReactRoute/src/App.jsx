9ccd798729290cfc6ad9403f7ea985232cee5538
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-10 12:13:56 +0530

    feat: Add routing with react-router-dom and create Blog components

--- 11-ReactRoute/src/App.jsx
+++ 11-ReactRoute/src/App.jsx
@@ -1,8 +1,31 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/blog' element={<Blog/>}  />
+          <Route path='/blog/1' element={<Blog1/>}  />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+function Blog(){
+  return (
+    <>
+      This is My blog
+    </>
+  )
+}
+function Blog1(){
+  return (
+    <>
+      This is My Blog1
+      <p>Whats up</p>
+    </>
   )
 }
 
-export default App
+export default App;
