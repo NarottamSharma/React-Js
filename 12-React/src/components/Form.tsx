c214d842b7873e5e2ebd65fecd52579fe49b61c3
Narottam Sharma <narottamphodegaa@gmail.com>
2025-09-15 12:10:39 +0530

    feat: initialize React project with Vite, TypeScript, and ESLint
    - Added package.json with dependencies for React, React DOM, and React Hook Form.
    - Created App component to render the Form component.
    - Implemented Form component using react-hook-form for form handling.
    - Set up main entry point with StrictMode and ReactDOM.createRoot.
    - Added vite-env.d.ts for Vite client types.
    - Configured TypeScript with tsconfig.app.json and tsconfig.node.json for app and node environments.
    - Created vite.config.ts for Vite configuration with React plugin.

---
+++ 12-React/src/components/Form.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const Form = () => {
+  const {register,handleSubmit,formState:{errors,isSubmit}} = useForm()
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Form
